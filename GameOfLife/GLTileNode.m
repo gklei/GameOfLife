7e4b9b65582253e43c7cf824811b79171f85126d
altonapple <altonlea@My-MacBook-Pro.local>
2013-11-08 13:34:04 -0500

    Various coloring routines.

--- GameOfLife/GLTileNode.m
+++ GameOfLife/GLTileNode.m
@@ -27,13 +27,27 @@
    return tile;
 }
 
+- (float)colorDistance
+{
+   CGPoint origin = self.position;
+   float dist = sqrt(origin.x * origin.x + origin.y * origin.y);
+   dist /= sqrt(320 * 320 + 480 * 480);
+   return dist;
+}
+
 - (SKColor *)getNextColor:(CrayolaColorName *)colorName
 {
-   *colorName = [SKColor getNextColorName:*colorName];
-   if (*colorName == _deadColorName)
-      *colorName = [SKColor getNextColorName:*colorName];
+//   *colorName = [SKColor getNextColorName:*colorName];
+//   if (*colorName == _deadColorName)
+//      *colorName = [SKColor getNextColorName:*colorName];
+//   
+//   return [SKColor colorForCrayolaColorName:*colorName];
    
-   return [SKColor colorForCrayolaColorName:*colorName];
+//   return [SKColor colorWithHue:[self colorDistance]
+//                     saturation:(arc4random()/((float)RAND_MAX * 2)) + 0.25
+//                     brightness:1.0
+//                          alpha:1.0];
+   return [SKColor colorWithRed:0.2 green:0.2 blue:[self colorDistance] alpha:1.0];
 }
 
 - (void)setIsLiving:(BOOL)living
