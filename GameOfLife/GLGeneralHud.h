6fdd37182d6efe1e520399167b492e7f52aa0fa7
Gregory Klein <gregoryhklein@gmail.com>
2013-11-24 22:48:07 -0500

    Created GeneralHud class

---
+++ GameOfLife/GLGeneralHud.h
@@ -0,0 +1,13 @@
+//
+//  GLGeneralHud.h
+//  GameOfLife
+//
+//  Created by Gregory Klein on 11/24/13.
+//  Copyright (c) 2013 Gregory Klein. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GLGeneralHud : SKNode
+
+@end
---
+++ GameOfLife/GLGeneralHud.m
@@ -0,0 +1,13 @@
+//
+//  GLGeneralHud.m
+//  GameOfLife
+//
+//  Created by Gregory Klein on 11/24/13.
+//  Copyright (c) 2013 Gregory Klein. All rights reserved.
+//
+
+#import "GLGeneralHud.h"
+
+@implementation GLGeneralHud
+
+@end
