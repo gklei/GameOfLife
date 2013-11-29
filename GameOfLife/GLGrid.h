c6ad3780a12dee7d877c3dca9a17efca511b5c18
Gregory Klein <gregoryhklein@gmail.com>
2013-11-29 12:53:19 -0500

    Added GLGrid class to handle grid-specific information

---
+++ GameOfLife/GLGrid.h
@@ -0,0 +1,13 @@
+//
+//  GLGrid.h
+//  GameOfLife
+//
+//  Created by Gregory Klein on 11/29/13.
+//  Copyright (c) 2013 Gregory Klein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLGrid : NSObject
+
+@end
---
+++ GameOfLife/GLGrid.m
@@ -0,0 +1,13 @@
+//
+//  GLGrid.m
+//  GameOfLife
+//
+//  Created by Gregory Klein on 11/29/13.
+//  Copyright (c) 2013 Gregory Klein. All rights reserved.
+//
+
+#import "GLGrid.h"
+
+@implementation GLGrid
+
+@end
