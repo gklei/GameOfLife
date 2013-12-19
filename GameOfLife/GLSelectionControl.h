efa3fc1a9725a955ff47bcfba8867cf02d476bd0
Gregory Klein <gregoryhklein@gmail.com>
2013-12-18 23:22:09 -0500

    Added files for GLSelectionControl

---
+++ GameOfLife/GLSelectionControl.h
@@ -0,0 +1,13 @@
+//
+//  GLSelectionControl.h
+//  GameOfLife
+//
+//  Created by Gregory Klein on 12/18/13.
+//  Copyright (c) 2013 Gregory Klein. All rights reserved.
+//
+
+#import "GLUIControl.h"
+
+@interface GLSelectionControl : GLUIControl
+
+@end
---
+++ GameOfLife/GLSelectionControl.m
@@ -0,0 +1,13 @@
+//
+//  GLSelectionControl.m
+//  GameOfLife
+//
+//  Created by Gregory Klein on 12/18/13.
+//  Copyright (c) 2013 Gregory Klein. All rights reserved.
+//
+
+#import "GLSelectionControl.h"
+
+@implementation GLSelectionControl
+
+@end
