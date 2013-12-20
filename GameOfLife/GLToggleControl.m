f8eaf038c12b2b06d13f17755ef68253d794df7a
Gregory Klein <gregoryhklein@gmail.com>
2013-12-20 00:30:24 -0500

    Initial work on color interpolation with toggle controls and sliding them

--- GameOfLife/GLToggleControl.m
+++ GameOfLife/GLToggleControl.m
@@ -22,11 +22,23 @@
    SKAction *_enableAnimation;
    SKAction *_disableAnimation;
 
+   SKColor *_innerRingDisabledColor;
+   SKColor *_innerRingEnabledColor;
+
+   SKColor *_outerRingDisabledColor;
+   SKColor *_outerRingEnabledColor;
+
+   float _leftXBound;
+   float _rightXBound;
+
    BOOL _animating;
    BOOL _firstTouchInHitBox;
 
    SKAction * _toggleSound;
    NSString * _preferenceKey;
+
+   float _innerRingOffsetInAccumulatedFrame;
+   float _innerRingSlidingRange;
 }
 @end
 
@@ -36,12 +48,28 @@
 {
    if (self = [super init])
    {
-      _state = e_TOGGLE_CONTROL_DISABLED;
-      _toggleSound = [SKAction playSoundFileNamed:@"button.press.wav" waitForCompletion:NO];
+      [self setupVariables];
       [self setupButtonImages];
       [self setupHitBox];
    }
    return self;
+}
+
+- (void)setupVariables
+{
+   _state = e_TOGGLE_CONTROL_DISABLED;
+   _toggleSound = [SKAction playSoundFileNamed:@"button.press.wav" waitForCompletion:NO];
+   _leftXBound = -INNER_RING_OFFSET_FROM_CENTER;
+   _rightXBound = INNER_RING_OFFSET_FROM_CENTER;
+
+   _innerRingDisabledColor = [SKColor crayolaCottonCandyColor];
+   _innerRingEnabledColor = [SKColor crayolaLimeColor];
+
+   _outerRingDisabledColor = [SKColor crayolaVioletRedColor];
+   _outerRingEnabledColor = [SKColor crayolaCaribbeanGreenPearlColor];
+
+   _innerRingSlidingRange = INNER_RING_OFFSET_FROM_CENTER * 2;
+   _innerRingOffsetInAccumulatedFrame = INNER_RING_OFFSET_FROM_CENTER;
 }
 
 - (id)initWithPreferenceKey:(NSString *)key
@@ -79,12 +107,12 @@
 {
    _outerRing = [SKSpriteNode spriteNodeWithImageNamed:@"toggle-ring-outer@2x.png"];
    _outerRing.colorBlendFactor = 1.0;
-   _outerRing.color = [SKColor crayolaVioletRedColor];
+   _outerRing.color = _outerRingDisabledColor;
 
    _innerRing = [SKSpriteNode spriteNodeWithImageNamed:@"radio-unchecked@2x.png"];
    [_innerRing setScale:.6];
    _innerRing.colorBlendFactor = 1.0;
-   _innerRing.color = [SKColor crayolaCottonCandyColor];
+   _innerRing.color = _innerRingDisabledColor;
    _innerRing.position = CGPointMake(_outerRing.position.x - INNER_RING_OFFSET_FROM_CENTER,
                                     _outerRing.position.y);
    [self addChild:_innerRing];
@@ -124,10 +152,10 @@
                                            y:0
                                     duration:TOGGLE_ANIMATION_DURATION];
 
-   SKAction *enableInnerRingColor = [SKAction colorizeWithColor:[SKColor crayolaLimeColor]
+   SKAction *enableInnerRingColor = [SKAction colorizeWithColor:_innerRingEnabledColor
                                                colorBlendFactor:1
                                                        duration:TOGGLE_COLOR_ANIMATION_DURATION];
-   SKAction *enableOuterRingColor = [SKAction colorizeWithColor:[SKColor crayolaCaribbeanGreenPearlColor]
+   SKAction *enableOuterRingColor = [SKAction colorizeWithColor:_outerRingEnabledColor
                                                colorBlendFactor:1
                                                        duration:TOGGLE_COLOR_ANIMATION_DURATION];
 
@@ -155,10 +183,10 @@
                                             y:0
                                      duration:TOGGLE_ANIMATION_DURATION];
    
-   SKAction *disableInnerRingColor = [SKAction colorizeWithColor:[SKColor crayolaCottonCandyColor]
+   SKAction *disableInnerRingColor = [SKAction colorizeWithColor:_innerRingDisabledColor
                                                 colorBlendFactor:1
                                                         duration:TOGGLE_COLOR_ANIMATION_DURATION];
-   SKAction *disableOuterRingColor = [SKAction colorizeWithColor:[SKColor crayolaVioletRedColor]
+   SKAction *disableOuterRingColor = [SKAction colorizeWithColor:_outerRingDisabledColor
                                                 colorBlendFactor:1
                                                         duration:TOGGLE_COLOR_ANIMATION_DURATION];
    
@@ -197,7 +225,85 @@
       [self runDisableAnimationsWithCompletion:completion];
       [self setState:e_TOGGLE_CONTROL_DISABLED];
    }
-//   NSLog(@"%@", [self stringValue]);
+}
+
+- (void)updateInnerRingPositionX:(float)x
+{
+   _innerRing.position = CGPointMake(x, _innerRing.position.y);
+   self.hitBox.position = _innerRing.position;
+}
+
+- (UIColor *)colorLerpFrom:(UIColor *)start
+                        to:(UIColor *)end
+              withDuration:(float)t
+{
+   if(t < 0.0f) t = 0.0f;
+   if(t > 1.0f) t = 1.0f;
+
+   const CGFloat *startComponent = CGColorGetComponents(start.CGColor);
+   const CGFloat *endComponent = CGColorGetComponents(end.CGColor);
+
+   float startAlpha = CGColorGetAlpha(start.CGColor);
+   float endAlpha = CGColorGetAlpha(end.CGColor);
+
+   float r = startComponent[0] + (endComponent[0] - startComponent[0]) * t;
+   float g = startComponent[1] + (endComponent[1] - startComponent[1]) * t;
+   float b = startComponent[2] + (endComponent[2] - startComponent[2]) * t;
+   float a = startAlpha + (endAlpha - startAlpha) * t;
+
+   return [UIColor colorWithRed:r green:g blue:b alpha:a];
+}
+
+- (void)moveInnerRingByDeltaX:(float)deltaX
+{
+   float percentage = (_innerRing.position.x + _innerRingOffsetInAccumulatedFrame) / _innerRingSlidingRange;
+   if (deltaX > 0)
+   {
+      _innerRing.color = [self colorLerpFrom:_innerRingDisabledColor
+                                          to:_innerRingEnabledColor
+                                withDuration:percentage];
+      _outerRing.color = [self colorLerpFrom:_outerRingDisabledColor
+                                          to:_outerRingEnabledColor
+                                withDuration:percentage];
+   }
+   else
+   {
+      _innerRing.color = [self colorLerpFrom:_innerRingEnabledColor
+                                          to:_innerRingDisabledColor
+                                withDuration:percentage];
+      _outerRing.color = [self colorLerpFrom:_outerRingEnabledColor
+                                          to:_outerRingDisabledColor
+                                withDuration:percentage];
+   }
+
+   [self updateInnerRingPositionX:_innerRing.position.x + deltaX];
+}
+
+- (void)handleTouchMoved:(UITouch *)touch
+{
+   float convertedX = [touch locationInNode:self].x;
+   float convertedPreviousX = [touch previousLocationInNode:self].x;
+   float deltaX = convertedX - convertedPreviousX;
+
+   if (_innerRing.position.x + deltaX <= _leftXBound)
+   {
+      _innerRing.color = _innerRingDisabledColor;
+      _outerRing.color = _outerRingDisabledColor;
+      [self updateInnerRingPositionX:_leftXBound];
+      self.state = e_TOGGLE_CONTROL_DISABLED;
+      return;
+   }
+
+   if (_innerRing.position.x + deltaX >= _rightXBound)
+   {
+      _innerRing.color = _innerRingEnabledColor;
+      _outerRing.color = _outerRingEnabledColor;
+      [self updateInnerRingPositionX:_rightXBound];
+      self.state = e_TOGGLE_CONTROL_ENABLED;
+      return;
+   }
+
+   [self moveInnerRingByDeltaX:deltaX];
 }
 
 - (void)handleTouchEnded:(UITouch *)touch
