9f8949ba5984fee018e7d68234b5818b462d8cde
Gregory Klein <gregoryhklein@gmail.com>
2014-01-13 18:30:47 -0500

    Removed some commented out code

--- GameOfLife/GLGridScene.m
+++ GameOfLife/GLGridScene.m
@@ -551,13 +551,6 @@
 
 - (void)screenShotButtonPressed
 {
-//   GLAlertLayer *alert = [[GLAlertLayer alloc] init];
-//   [alert addHeaderText:@"header 1"];
-//   [alert addHeaderText:@"header 2"];
-//   [alert addBodyText:@"This is a body!"];
-//
-//   alert.position = CGPointMake(0, self.size.height - 40);
-//   [self addChild:alert];
    [self takeScreenShot];
 }
 
