924904f41ac6eb94f5229756d43f0e7d39121a95
Gregory Klein <gregoryhklein@gmail.com>
2014-01-28 19:03:18 -0500

    Added scanner animation class

---
+++ GameOfLife/GLScannerAnimation.h
@@ -0,0 +1,13 @@
+//
+//  GLScannerAnimation.h
+//  GameOfLife
+//
+//  Created by Gregory Klein on 1/28/14.
+//  Copyright (c) 2014 Gregory Klein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLScannerAnimation : NSObject
+
+@end
---
+++ GameOfLife/GLScannerAnimation.m
@@ -0,0 +1,13 @@
+//
+//  GLScannerAnimation.m
+//  GameOfLife
+//
+//  Created by Gregory Klein on 1/28/14.
+//  Copyright (c) 2014 Gregory Klein. All rights reserved.
+//
+
+#import "GLScannerAnimation.h"
+
+@implementation GLScannerAnimation
+
+@end
