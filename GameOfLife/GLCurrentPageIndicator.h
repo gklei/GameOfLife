df0109c40cc0453703e36d0ba974cf40e18ea5b9
Gregory Klein <gregoryhklein@gmail.com>
2014-02-06 23:03:42 -0500

    Added new scanner sound fx, added classes for current page indication, added new image to represent current page.

---
+++ GameOfLife/GLCurrentPageIndicator.h
@@ -0,0 +1,13 @@
+//
+//  GLCurrentPageIndicator.h
+//  GameOfLife
+//
+//  Created by Gregory Klein on 2/6/14.
+//  Copyright (c) 2014 Gregory Klein. All rights reserved.
+//
+
+#import "GLUIButton.h"
+
+@interface GLCurrentPageIndicator : GLUIButton
+
+@end
---
+++ GameOfLife/GLCurrentPageIndicator.m
@@ -0,0 +1,13 @@
+//
+//  GLCurrentPageIndicator.m
+//  GameOfLife
+//
+//  Created by Gregory Klein on 2/6/14.
+//  Copyright (c) 2014 Gregory Klein. All rights reserved.
+//
+
+#import "GLCurrentPageIndicator.h"
+
+@implementation GLCurrentPageIndicator
+
+@end
