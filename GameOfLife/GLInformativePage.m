6762e8092fb27b0e191b86a6b3a9c1890b8bbbe9
Gregory Klein <gregoryhklein@gmail.com>
2014-03-02 13:46:26 -0500

    Slight updates to text in the about section.

--- GameOfLife/GLInformativePage.m
+++ GameOfLife/GLInformativePage.m
@@ -48,9 +48,9 @@
 
    [importPhotoPage addHeaderText:@"IMPORT PHOTO"];
    [importPhotoPage addNewLines:1];
-   [importPhotoPage addBodyText:@"Try loading a previously captured LiFE state by pressing and holding the Camera button!  You will be taken to your Photo Library where you can access whichever photo you would like to turn to LiFE."];
+   [importPhotoPage addBodyText:@"Try loading a previously captured LiFE state by pressing and holding the Camera button.  You will be taken to your Photo Library where you can access whichever photo you would like to turn to LiFE."];
    [importPhotoPage addNewLines:1];
-   [importPhotoPage addBodyText:@"This feature may be used with any picture in your Photo Library, but it will work the best with a picture that was taken by pressing the camera button at the bottom."];
+   [importPhotoPage addBodyText:@"This feature may be used with any picture in your Photo Library, but it will work the best with a picture that was captured from this app."];
 
    return importPhotoPage;
 }
@@ -61,7 +61,7 @@
 
    [sharePhotoPage addHeaderText:@"SHARE PHOTO"];
    [sharePhotoPage addNewLines:1];
-   [sharePhotoPage addBodyText:@"Share a saved LiFE state with your friends by pressing and holding the Restore button!"];
+   [sharePhotoPage addBodyText:@"Share a saved LiFE state with your friends by pressing and holding the Restore button."];
    [sharePhotoPage addNewLines:1];
    [sharePhotoPage addBodyText:@"You will be taken to your messages app where you can add the repient(s) who you wish to share your LiFE state with."];
 
